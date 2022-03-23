e2bc5809b2875a7b018a100bdcdc3db8cc53a106
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2022-03-23 13:28:43 +0530

    2nd Iteration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,52 @@
 import React, { useState } from "react";
+import ToDoList from "./ToDoList";
+import InputArea from "./InputArea";
 
 function App() {
 
-    const[inputText, setInputText] = useState("");
-    const[items, setItems] = useState([])
+    
+    const [items, setItems] = useState([])
 
-    function handleChange(event){
-        const newValue = event.target.value;
-        setInputText(newValue)
-    }
+    
 
-    function handleChange1(){
+    function handleChange1(inputText) {
         setItems(prevValue => {
-            return [...prevValue, inputText]
+            return [...prevValue, inputText];
         })
     }
 
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" onChange={handleChange} />
-        <button type="submit" onClick={handleChange1}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {items.map(item => {
-              return <li>{item}</li>
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+    function deleteItem(id){
+        setItems((prevValue) => {
+            return prevValue.filter(
+                (item, index) => {
+                    return index !== id;
+                }
+            )
+        })
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <InputArea 
+            addItem={handleChange1}
+            />
+            <div>
+                <ul>
+                    {items.map((item, index) => (
+                        <ToDoList 
+                        key={index}
+                        id={index}
+                        text={item}
+                        onChecked={deleteItem}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
